02c95a0674dbcab0bd7d6c4eef4327c3857db706
Jigish Belani <jigish.belani.external@bunge.com>
2025-07-29 17:32:27 -0500

    Initial Commit

---
+++ MiningStats/MiningStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiningStatsApp.swift
+//  MiningStats
+//
+//  Created by Jigish Belani on 7/29/25.
+//
+
+import SwiftUI
+
+@main
+struct MiningStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MiningStatsTests/MiningStatsTests.swift
@@ -0,0 +1,17 @@
+//
+//  MiningStatsTests.swift
+//  MiningStatsTests
+//
+//  Created by Jigish Belani on 7/29/25.
+//
+
+import Testing
+@testable import MiningStats
+
+struct MiningStatsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
